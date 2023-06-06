d4c49010a10f937c78930c23a0e6624dff97ffdd
igorunderplayer <igorschug6@gmail.com>
2023-06-06 18:24:38 -0300

    update updates

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
+import * as Updates from 'expo-updates'
+
+import { useEffect } from 'react'
+
 import { SummonerProvider } from './src/hooks/summoner'
 import { Routes } from './src/routes'
 
 import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
+  useEffect(() => {
+    return Updates.addListener((event) => {
+      if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
+        alert('An update is available. Restart the app to continue')
+        Updates.reloadAsync()
+      }
+    })
+  }, [])
+
+
   return (
     <SummonerProvider>
       <Routes />
